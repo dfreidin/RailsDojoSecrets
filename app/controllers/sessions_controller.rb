9e71380475898192fb1da1393fbcd32ee80afc84
dfreidin <36039896+dfreidin@users.noreply.github.com>
2018-04-10 15:38:20 -0700

    authentication 1: added the ability to log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+    # render login page
+  end
+  def create
+      # Log User In
+      @user = User.find_by_email(params[:user][:email]).try(:authenticate, params[:user][:password])
+      if @user
+        session[:user_id] = @user.id
+        redirect_to "/users/#{@user.id}"
+      else
+        flash[:errors] = ["Invalid Combination"]
+        redirect_to "/sessions/new"
+      end
+  end
+  def destroy
+      # Log User out
+      session[:user_id] = nil
+      redirect_to "/sessions/new"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/:id' => 'users#show'
+
+  get 'sessions/new'
+
+  post 'users' => 'sessions#create'
+
+  delete 'sessions/:id' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
